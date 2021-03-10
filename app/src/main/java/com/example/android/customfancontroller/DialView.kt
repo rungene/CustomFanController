f0d7e3e682ac23836f6b9453edeccb3cb7b3d6cd
rungene <rungenesamuel@gmail.com>
2021-03-10 05:23:45 +0300

    inside the DialView class definition, initialize a Paint object with a handful of basic styles.

--- app/src/main/java/com/example/android/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/android/customfancontroller/DialView.kt
@@ -1,7 +1,9 @@
 package com.example.android.customfancontroller
 
 import android.content.Context
+import android.graphics.Paint
 import android.graphics.PointF
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 
@@ -30,9 +32,20 @@ class DialView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+   /* These values are created and initialized here instead of when the view is actually drawn to
+    ensure that the actual drawing step runs as fast as possible.*/
     //define several variables you need in order to draw the custom view
     private var radius = 0.0f                   // Radius of the circle.
     private var fanSpeed = FanSpeed.OFF         // The active selection.
     // position variable which will be used to draw label and indicator circle position
     private val pointPosition: PointF = PointF(0.0f, 0.0f)
+
+    //initialize a Paint object with a handful of basic styles.
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create( "", Typeface.BOLD)
+    }
 }
